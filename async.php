f228a7ab9b556b52ef0c34caa66669b0863ffbd3
mehdi <bagherii.mahdii@gmail.com>
2018-09-02 16:54:17 +0200

    async request

---
+++ async.php
@@ -0,0 +1,26 @@
+<?php
+require  'vendor\autoload.php';
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Psr7\Response;
+use GuzzleHttp\Exception\RequestException;
+
+$client = new Client();
+
+$promise = $client->requestAsync(
+    'GET',
+    'http://jsonplaceholder.typicode.com/posts/1'
+);
+
+$promise->then(
+  function (Response $response)
+  {
+      echo $response->getBody();
+  },
+  function (RequestException $exception)
+  {
+      echo $exception->getMessage();
+  }
+);
+$promise->wait();
+
