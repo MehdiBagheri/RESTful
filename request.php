2e5557cd7b87828420feb9d1a8cab0021c994d1f
mehdi <bagherii.mahdii@gmail.com>
2018-09-02 16:44:28 +0200

    basic request

---
+++ request.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Filename: ${NAME}.
+ * User: Mehdi Bagheri
+ * Date: Sep, 2018
+ */
+
+require  'vendor\autoload.php';
+
+use GuzzleHttp\Client;
+
+$client = new Client(['base_uri' => 'http://jsonplaceholder.typicode.com/']);
+
+$response = $client->get(
+    'posts/1'
+);
+
+var_dump($response);
+
+echo $response->getBody();
+
+$response = $client->get(
+    'posts/2'
+);
+
+var_dump($response);
+
+echo $response->getBody();
+
+$response = $client->get(
+    'comments/1'
+);
+
+var_dump($response);
+
+echo $response->getBody();
+$response = $client->get(
+    'https://httpbin.org/ip'
+);
+
+var_dump($response);
+
+echo $response->getBody();
