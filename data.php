259b6488f3421e6d1e76204db40d19dc152650e3
mehdi <bagherii.mahdii@gmail.com>
2018-09-02 18:30:55 +0200

    sending request data

---
+++ data.php
@@ -0,0 +1,22 @@
+<?php
+require  'vendor\autoload.php';
+
+use GuzzleHttp\Client;
+
+$client = new Client();
+
+$response = $client->request(
+    'POST',
+    'http://jsonplaceholder.typicode.com/posts',
+    [
+        'json' => [
+            'title' => 'Guzzle and REST',
+            'body' => 'Guzzle makes working whit REST APIs easy.',
+            'userId' =>2
+        ],
+    ]
+);
+
+var_dump($response);
+
+echo $response->getBody();
